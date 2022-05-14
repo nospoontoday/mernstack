73220a4962ef65678fff758b4ab6f17af39f1475
Oliver <oliverjohnpr2013@gmail.com>
2022-05-14 15:05:09 +0800

    Delete User/Profile

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -118,7 +118,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-// @route       GET apo/profile/user/:user_id
+// @route       GET api/profile/user/:user_id
 // @desc        Get profile by user ID
 // @access      Public
 router.get('/user/:user_id', async (req, res) => {
@@ -136,6 +136,26 @@ router.get('/user/:user_id', async (req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+// @route       DELETE api/profile
+// @desc        Delete profile, user & posts
+// @access      Private
+
+router.delete('/', auth, async (req, res) => {
+    try {
+        // @todo - remove user's posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id  });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+
+        res.json({ msg: 'User successfully deleted.' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 
 module.exports = router;
